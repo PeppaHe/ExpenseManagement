9d735c5beb61bca273ac1bd46ed224068fe7a501
lisahe <1299943745@qq.com>
2020-09-12 00:04:46 +0800

    CURD of department(finished)

---
+++ src/main/test/Test.java
@@ -0,0 +1,15 @@
+/**
+ * @author: hq
+ * @create time:2020/9/11 21:51
+ * @desc:
+ */
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 1;i < 10; i++){
+            for (int j = i; j < 10; j++){
+                System.out.print(i+"*"+j+"="+i*j+" ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
