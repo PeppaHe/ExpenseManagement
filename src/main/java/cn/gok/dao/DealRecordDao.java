cde47cf9c3c619ec0273000a0819a7b93c648386
lisahe <1299943745@qq.com>
2020-09-12 15:43:12 +0800

    started main part

---
+++ src/main/java/cn/gok/dao/DealRecordDao.java
@@ -0,0 +1,11 @@
+package cn.gok.dao;
+
+import cn.gok.entity.DealRecord;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository("dealRecordDao")
+public interface DealRecordDao {
+    void insert(DealRecord dealRecord);
+    List<DealRecord> selectByClaimVoucher(int cvid);
+}
